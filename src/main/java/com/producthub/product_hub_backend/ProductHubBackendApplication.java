3531dab4995726fb0a5f7deaa2b8936446bd2104
ngarre <a28710@svalero.com>
2026-09-11 17:39:22 +0200

    chore: initialize Spring Boot project

---
+++ src/main/java/com/producthub/product_hub_backend/ProductHubBackendApplication.java
@@ -0,0 +1,13 @@
+package com.producthub.product_hub_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductHubBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductHubBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/producthub/product_hub_backend/ProductHubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.producthub.product_hub_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductHubBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
